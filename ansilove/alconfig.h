a48780a5a2612800ddfbcfdcee4cba53d7283606
ByteProject <stefan.vogt@byteproject.net>
2011-12-21 10:56:45 +0100

    added alconfig as C implementation of ansilove.cfg.php

---
+++ ansilove/alconfig.h
@@ -0,0 +1,41 @@
+//
+//  alconfig.h
+//  AnsiLove/ObjC
+//
+//  Copyright (c) 2011, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
+//
+
+#define ANSILOVE_FILES_DIRECTORY "ansis/"
+
+#define ANSILOVE_LOG_FILE "ansilove.log"
+
+#define PCBOARD_STRIP_CODES "@POFF@,@WAIT@"
+
+#define DIZ_EXTENSIONS ".diz,.ion"
+
+#define SUBSTITUTE_BREAK "1"
+#define WRAP_COLUMN_80 "1"
+
+#define CED_BACKGROUND_COLOR "170,170,170"
+#define CED_FOREGROUND_COLOR "0,0,0"
+
+#define WORKBENCH_COLOR_0 "170,170,170"
+#define WORKBENCH_COLOR_1 "0,0,255"
+#define WORKBENCH_COLOR_2 "255,255,255"
+#define WORKBENCH_COLOR_3 "0,255,255"
+#define WORKBENCH_COLOR_4 "0,0,0"
+#define WORKBENCH_COLOR_5 "255,0,255"
+#define WORKBENCH_COLOR_6 "102,136,187"
+#define WORKBENCH_COLOR_7 "255,255,255"
+
+#define THUMBNAILS_SIZE "1"
+#define THUMBNAILS_HEIGHT "0"
+#define THUMBNAILS_TAG "-thumbnail"
+
+#define SPLIT "0"
+#define SPLIT_HEIGHT "4096"
+#define SPLIT_SEPARATOR "."
