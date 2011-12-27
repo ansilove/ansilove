26f16e112b23852a713afef414fa71e722709229
ByteProject <stefan.vogt@byteproject.net>
2011-12-27 10:41:12 +0100

    importing all the other stuff we've implemented so far

--- ansilove/ansilove.h
+++ ansilove/ansilove.h
@@ -12,10 +12,16 @@
 #if defined(__APPLE__) && defined(__MACH__)
 #import <Foundation/Foundation.h>
 #import <gd.h>
+#import "alconfig.h"
+#import "strtolower.h"
+#import "substr.h"
 #else
 #include <stdio.h>
 #include <stdlib.h>
 #include <gd.h>
+#include "alconfig.h"
+#include "strtolower.h"
+#include "substr.h"
 #endif
 
 #ifndef ansilove_h
