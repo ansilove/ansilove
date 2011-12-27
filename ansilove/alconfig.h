b154b585ef4587d53dad50c2dc0350ec3d448780
ByteProject <stefan.vogt@byteproject.net>
2011-12-27 11:02:57 +0100

    added define for the fonts directory
    When installed as command line tool you surely don't want the fonts to
    be in /usr/bin so we provide a define to change the path of it. Also
    changed to a more UNIX-like path define.

--- ansilove/alconfig.h
+++ ansilove/alconfig.h
@@ -15,7 +15,9 @@
 // configuration defines
 #define VERSION "1.0.0"
 
-#define ANSILOVE_FILES_DIRECTORY "ansis/"
+#define ANSILOVE_FONT_DIRECTORY "/fonts"
+
+#define ANSILOVE_FILES_DIRECTORY "/ansis"
 
 #define ANSILOVE_LOG_FILE "ansilove.log"
 
