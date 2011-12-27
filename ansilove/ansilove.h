96ec0b557337e01a7cf67f865c6823dac0f19611
ByteProject <stefan.vogt@byteproject.net>
2011-12-27 09:37:34 +0100

    ansilove.h for prototypes, properly importing <gd.h>

---
+++ ansilove/ansilove.h
@@ -0,0 +1,28 @@
+//
+//  ansilove.h
+//  AnsiLove/C
+//
+//  Copyright (c) 2011, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
+//
+
+#if defined(__APPLE__) && defined(__MACH__)
+#import <Foundation/Foundation.h>
+#import <gd.h>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#include <gd.h>
+#endif
+
+#ifndef ansilove_h
+#define ansilove_h
+
+// prototypes
+
+
+
+#endif
