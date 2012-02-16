8973a4dec593d14c3bb7c2c2f10fd50f400c5ecd
ByteProject <stefan.vogt@byteproject.net>
2012-02-16 01:18:33 +0100

    now linking to libmath when building

--- shbuild/build_ubuntu.sh
+++ shbuild/build_ubuntu.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 #########################################################
 # This UNIX script builds AnsiLove/C under Ubuntu Linux #
-# and links it against the GD Graphics Library.	        #
+# and links it against libgd and libmath.		        #
 #########################################################
 # modify alconfig.h to configure the resulting binary   #
 #########################################################
-# build script version 1.0, created by Stefan Vogt      #
+# build script version 1.1, created by Stefan Vogt      #
 #########################################################
 
 echo "========================================="
 echo "AnsiLove/C build script --> Ubuntu Linux "
 echo "========================================="
 echo "... building"
-gcc -Wall -I../lib/MacOSX/libgd/include/ ../ansilove/main.c ../ansilove/alconfig.h ../ansilove/ansilove.h ../ansilove/ansilove.c ../ansilove/substr.h ../ansilove/substr.c ../ansilove/strtolower.h ../ansilove/strtolower.c ../ansilove/explode.h ../ansilove/explode.c ../ansilove/filesize.h ../ansilove/filesize.c  /usr/lib/libgd.so.2 -o ansilove
+gcc -Wall -I../lib/MacOSX/libgd/include/ ../ansilove/main.c ../ansilove/alconfig.h ../ansilove/ansilove.h ../ansilove/ansilove.c ../ansilove/substr.h ../ansilove/substr.c ../ansilove/strtolower.h ../ansilove/strtolower.c ../ansilove/explode.h ../ansilove/explode.c ../ansilove/filesize.h ../ansilove/filesize.c  /usr/lib/libgd.so.2 -lm -o ansilove
 echo "... done."
 
 exit
