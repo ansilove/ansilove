c78b603ff790a17af4098cff6154029b0aff117c
h3rb <hgilliland@gmail.com>
2013-01-02 10:55:44 -0800

    Create build_ubuntu_fo_real.sh
    Revised for gd2, to compile on Ubuntu lamp stacks

---
+++ shbuild/build_ubuntu_fo_real.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#########################################################
+# This UNIX script builds AnsiLove/C under Ubuntu Linux #
+# and links it against libgd.                           #
+#########################################################
+# modify alconfig.h to configure the resulting binary   #
+#########################################################
+# build script version 1.2, updated by h3rb gi11iland   #
+# originally created by Stefan Vogt                     #
+#########################################################
+
+echo "========================================="
+echo "AnsiLove/C build script --> Ubuntu Linux "
+echo "========================================="
+echo "... building"
+gcc -Wall -lgd2 -ljpeg -lpng -lz -lfreetype -lm ../ansilove/main.c \
+        ../ansilove/alconfig.h ../ansilove/ansilove.h ../ansilove/ansilove.c \
+        ../ansilove/substr.h ../ansilove/substr.c ../ansilove/strtolower.h \
+        ../ansilove/strtolower.c ../ansilove/explode.h ../ansilove/explode.c \
+        ../ansilove/filesize.h ../ansilove/filesize.c ../ansilove/albinfonts.h \
+        ../ansilove/albinfonts.c -o ansilove
+echo "... done.  Get on with that looooove!"
+
+exit
