132915879b72fff85d87a02c36a8affb20914a43
Brian Cassidy <bricas@cpan.org>
2013-01-03 00:22:55 -0400

    replace old build script. redo README info for build script.

--- shbuild/build_ubuntu.sh
+++ shbuild/build_ubuntu.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 #########################################################
 # This UNIX script builds AnsiLove/C under Ubuntu Linux #
-# and links it against libgd.							#
+# and links it against libgd.                           #
 #########################################################
 # modify alconfig.h to configure the resulting binary   #
 #########################################################
-# build script version 1.1, created by Stefan Vogt      #
+# build script version 1.2, updated by h3rb gi11iland   #
+# originally created by Stefan Vogt                     #
 #########################################################
 
 echo "========================================="
 echo "AnsiLove/C build script --> Ubuntu Linux "
 echo "========================================="
 echo "... building"
-gcc -Wall -I../lib/MacOSX/libgd/include/ ../ansilove/main.c ../ansilove/alconfig.h ../ansilove/ansilove.h ../ansilove/ansilove.c ../ansilove/substr.h ../ansilove/substr.c ../ansilove/strtolower.h ../ansilove/strtolower.c ../ansilove/explode.h ../ansilove/explode.c ../ansilove/filesize.h ../ansilove/filesize.c ../ansilove/albinfonts.h ../ansilove/albinfonts.c /usr/lib/libgd.so.2 -o ansilove
-echo "... done."
+mkdir -p ../bin
+gcc -Wall ../ansilove/main.c \
+        ../ansilove/alconfig.h ../ansilove/ansilove.h ../ansilove/ansilove.c \
+        ../ansilove/substr.h ../ansilove/substr.c ../ansilove/strtolower.h \
+        ../ansilove/strtolower.c ../ansilove/explode.h ../ansilove/explode.c \
+        ../ansilove/filesize.h ../ansilove/filesize.c ../ansilove/albinfonts.h \
+        ../ansilove/albinfonts.c \
+ -lz -ljpeg -lpng -lfreetype -lm -lgd \
+ -o ../bin/ansilove
+echo "... done.  Get on with that looooove!"
 
 exit
