cd5076feccedd13787efa58955744da8803fc24e
Frederic Cambus <fred@statdns.com>
2016-01-16 18:11:12 +0100

    Removed prototype for 'str_replace' which has been pruned

--- src/ansilove.h
+++ src/ansilove.h
@@ -32,9 +32,6 @@ void alDrawChar(gdImagePtr im, const unsigned char *font_data, int32_t int_bits,
                 int32_t font_size_y, int32_t position_x, int32_t position_y, 
                 int32_t color_background, int32_t color_foreground, unsigned char character);
 
-// helper functions
-char *str_replace(const char *string, const char *substr, const char *replacement);
-
 // character structures
 struct pcbChar {
     int32_t position_x;
