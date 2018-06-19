eb24b291b23b99fb281da8389f6909c2dcf2b2b6
Frederic Cambus <fred@statdns.com>
2018-06-19 12:05:59 +0200

    Increment maximum ansi sequences length

--- src/loaders/ansi.c
+++ src/loaders/ansi.c
@@ -121,7 +121,7 @@ void ansi(unsigned char *inputFileBuffer, int32_t inputFileSize, char *outputFil
         // ANSi sequence
         if (current_character == 27 && next_character == 91)
         {
-            for (ansi_sequence_loop = 0; ansi_sequence_loop < 12; ansi_sequence_loop++)
+            for (ansi_sequence_loop = 0; ansi_sequence_loop < 14; ansi_sequence_loop++)
             {
                 ansi_sequence_character = inputFileBuffer[loop + 2 + ansi_sequence_loop];
 
