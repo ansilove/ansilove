1366af743279eb94c597236182bf92737f4fbaab
Frederic Cambus <fred@statdns.com>
2020-05-24 10:32:09 +0200

    Define timespecsub macro in compat.h if it doesn't exists.

--- compat/compat.h
+++ compat/compat.h
@@ -9,4 +9,16 @@
 #include "strtonum.h"
 #endif
 
+#ifndef timespecsub
+#define	timespecsub(tsp, usp, vsp)					\
+	do {								\
+		(vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;		\
+		(vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;	\
+		if ((vsp)->tv_nsec < 0) {				\
+			(vsp)->tv_sec--;				\
+			(vsp)->tv_nsec += 1000000000L;			\
+		}							\
+	} while (0)
+#endif
+
 #endif /* COMPAT_H */
