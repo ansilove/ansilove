ac0bf4a0354f06890253fe45a602096a39c2f5a0
Tom Aylott <oblivious@subtlegradient.com>
2025-09-18 19:13:27 -0400

    Add Nix flake dev shell.

--- flake.nix
+++ flake.nix
@@ -12,29 +12,22 @@
         pkgs = import nixpkgs { inherit system; };
         inherit (pkgs) lib;
         libansilove = pkgs.libansilove;
-        libansiloveDev = libansilove.dev or libansilove;
         gd = pkgs.gd;
-        gdDev = gd.dev or gd;
-        prefixInputs = [ libansiloveDev gdDev pkgs.zlib.dev pkgs.libpng.dev ];
-        pkgConfigInputs = [ libansiloveDev gdDev pkgs.libpng.dev pkgs.zlib.dev ];
+        libs = map lib.getLib [ libansilove gd pkgs.libpng pkgs.zlib ];
+        devs = map lib.getDev [ libansilove gd pkgs.libpng pkgs.zlib ];
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
+          nativeBuildInputs = with pkgs; [
             cmake
             ninja
             pkg-config
             clang
             clang-tools
-            libansilove
-            gd
-            zlib
-            libpng
           ];
-
-          shellHook = ''
-            export CMAKE_PREFIX_PATH="${lib.makeSearchPath "" prefixInputs}${CMAKE_PREFIX_PATH:+:}$CMAKE_PREFIX_PATH"
-            export PKG_CONFIG_PATH="${lib.makeSearchPath "lib/pkgconfig" pkgConfigInputs}${PKG_CONFIG_PATH:+:}$PKG_CONFIG_PATH"
-          '';
+          buildInputs = devs ++ libs;
+          CMAKE_PREFIX_PATH = lib.makeSearchPath "" devs;
+          CMAKE_LIBRARY_PATH = lib.makeSearchPath "lib" libs;
+          PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" devs;
         };
       });
 }
